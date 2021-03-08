6321d270184eca16b0ed4507d5bd448add440b07
github username <github email address>
2021-03-08 11:39:35 -0500

    userpage work

--- src/Userpage.js
+++ src/Userpage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 
 function Userpage({ currentUser }) {
-  //   console.log(currentUser);
+  console.log(currentUser);
   return (
     <div>
       <Navbar currentUser={currentUser} />
